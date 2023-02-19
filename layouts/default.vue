9d0e91bb953184fd753ee8beb8001b04464275cb
Arif9878 <arifboyz16@gmail.com>
2023-02-19 20:40:55 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="app" id="app"
+  >
+    <!-- Render contents with transition -->
+    <transition name="fade" mode="out-in">
+      <Nuxt />
+    </transition>
+
+    <!-- App footer -->
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import feather from "feather-icons";
+import Navbar from "../components/Navbar.vue";
+export default {
+  data: () => {
+    return {
+      // Todo
+    };
+  },
+  mounted() {
+    feather.replace();
+  },
+  components: { Navbar },
+};
+</script>
+
+<style>
+.vue-back-to-top {
+  @apply p-2 sm:p-4 bg-indigo-500 hover:bg-indigo-600 text-white;
+  border-radius: 50%;
+  font-size: 22px;
+  line-height: 22px;
+}
+
+.fade-enter-active {
+  animation: coming 0.4s;
+  animation-delay: 0.2s;
+  opacity: 0;
+}
+
+.fade-leave-active {
+  animation: going 0.4s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+
+  to {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+}
+
+@keyframes coming {
+  from {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+
+  to {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app" id="app">
+  </div>
+</template>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
